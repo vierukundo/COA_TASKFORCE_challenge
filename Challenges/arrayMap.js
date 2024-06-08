aacb90a42147c0f14c2ef4a2b6f0103bc8378022
vierukundo <vierukundo@gmail.com>
2024-06-08 20:17:41 +0200

    Improved challenges code using semistandard style

--- Challenges/arrayMap.js
+++ Challenges/arrayMap.js
@@ -1,25 +1,23 @@
-function ArrayManipulation(arr, target) {
-    let sum = 0;
-    let start = 0;
+function ArrayManipulation (arr, target) {
+  let sum = 0;
+  let start = 0;
 
-    for (let end = 0; end < arr.length; end++) {
-        sum += arr[end];
-        console.log(sum)
+  for (let end = 0; end < arr.length; end++) {
+    sum += arr[end];
 
-        // Shrink the window as long as the sum is greater than the target
-        while (sum > target && start <= end) {
-            sum -= arr[start];
-            console.log("In if block:", sum);
-            start++;
-        }
-
-        // Check if the current window sum is equal to the target
-        if (sum === target) {
-            return true;
-        }
+    // decrease the sum and the subarray size by incrimenting start as long as the sum is greater than the target
+    while (sum > target && start <= end) {
+      sum -= arr[start];
+      start++;
     }
 
-    return false;
+    // Check if the current subarray sum is equal to the target
+    if (sum === target) {
+      return true;
+    }
+  }
+
+  return false;
 }
 
 // Example usage:
--- Challenges/stringTransform.js
+++ Challenges/stringTransform.js
@@ -1,27 +1,27 @@
-function stringTransform(str) {
-    const stringLength = str.length;
-    if (stringLength % 15 === 0) {
-        let reversedString = str.split("").reverse().join("");
-        const asciiValues = [];
-        for (let i = 0; i < reversedString.length; i++) {
-            const ascii = reversedString.charCodeAt(i);
-            asciiValues.push(ascii);
-        }
-        let newString = asciiValues.join(" ");
-        return newString
-    } else if (stringLength % 3 === 0) {
-        return str.split("").reverse().join("");
-    } else if (stringLength % 5 === 0) {
-        const asciiValues = [];
-        for (let i = 0; i < str.length; i++) {
-            const ascii = str.charCodeAt(i);
-            asciiValues.push(ascii);
-        }
-        let newString = asciiValues.join(" ");
-        return newString
-    } else {
-        return "";
+function stringTransform (str) {
+  const stringLength = str.length;
+  if (stringLength % 15 === 0) {
+    const reversedString = str.split('').reverse().join('');
+    const asciiValues = [];
+    for (let i = 0; i < reversedString.length; i++) {
+      const ascii = reversedString.charCodeAt(i);
+      asciiValues.push(ascii);
     }
+    const newString = asciiValues.join(' ');
+    return newString;
+  } else if (stringLength % 3 === 0) {
+    return str.split('').reverse().join('');
+  } else if (stringLength % 5 === 0) {
+    const asciiValues = [];
+    for (let i = 0; i < str.length; i++) {
+      const ascii = str.charCodeAt(i);
+      asciiValues.push(ascii);
+    }
+    const newString = asciiValues.join(' ');
+    return newString;
+  } else {
+    return '';
+  }
 }
 
-console.log(stringTransform("Hamburger"));
+console.log(stringTransform('Hamburger'));
